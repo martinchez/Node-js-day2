56e2b099a898408415f7ad6d851681f732ad6485
martinchez <martinchege1998@gmail.com>
2020-04-03 11:44:52 +0300

    update emails.js

--- email.js
+++ email.js
@@ -1,15 +1,23 @@
-var nodemailer= require('nodemailer');
-var transport = nodemailer.createTransport({
-    service: 'gmail',
+//start by add nodemailer modules to assit you im sending mails
+var nodemailer = require('nodemailer');
+var transporter= nodemailer.createTransport({
+    service:'gmail',
     auth:{
-        user:'admin@servdev.co.ke',
-        pass:'martinwaish1'
+        user:'admin@servdev.co.ke',//enter your email
+        pass:'admin123'//enter your password of your actual email
     }
 });
+var mailOptions ={
+    from:'your from email',
+    to:'the other email',
+    subject:'testing sending email with nodemailer',
+    text:'Learning node js with @creative minds'
+};
 
-var mailOptions={
-    from:'admin@servdev.co.ke',
-    to: 'martinchege1998@gmail.com',
-    subject: 'using nodemailer to send emails',
-    text: ''
-}
+transporter.sendMail(mailOptions , function (error,info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent :' + info.response);
+    }
+});
