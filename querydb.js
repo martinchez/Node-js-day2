f286c8ae94afe8748815adb0596ea3c36654e646
martinchez <martinchege1998@gmail.com>
2020-04-03 12:27:01 +0300

    added query js and saved the video tutorial of the same projec

--- querydb.js
+++ querydb.js
@@ -1,13 +1,16 @@
+//we are going to query fro the database using node js
 var mysql= require('mysql');
-var con = mysql.createConnection({
-    host:'localhost',
+
+var con= mysql.createConnection({
+    host:"localhost",
     user:"root",
     password:""
 });
+
 con.connect(function (err) {
     if(err) throw err;
-    console.log("connected");
-    con.query(sql,function (err,result) {
+    console.log('connected!');
+    con.query(sql, function (err,result) {
         if(err) throw err;
         console.log("result:" + result);
     });
